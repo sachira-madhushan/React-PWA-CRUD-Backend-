c8e00264c43d0192a0b7ddb838698933df39e943
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-04-22 15:13:16 +0530

    check origin for local host

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://pwa-crud-new-auth.netlify.app',
+  origin: 'http://localhost:5173/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
