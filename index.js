616e11a6824141cfc5b2ecc54d344f66899df26d
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-04-22 17:06:26 +0530

    removed options method in cors

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ dotenv.config();
 const app = express();
 app.use(cors({
   origin: 'http://localhost:4173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
-//app.options('*', cors());
+app.options('/api/*', cors());
 
 const port = 4000;
 
